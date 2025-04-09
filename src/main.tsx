97d5a628cf06e2d118638fa6efd638b2851c6dc2
Jérémy <145568190+Jyraven@users.noreply.github.com>
2025-04-09 17:24:42 +0200

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { HashRouter } from 'react-router-dom';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HashRouter basename="/My_Portfolio">
+    <HashRouter>
       <App />
     </HashRouter>
-  </StrictMode>
 );
